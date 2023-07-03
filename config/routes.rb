8abd6e5287ce074916867429d8c8bc21e0978456
Elson Otake <elsonotake@yahoo.com.br>
2023-07-03 16:24:30 -0300

    Create route to create action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :products
+  post "/checkout", to: "checkout#create"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

0f289d84bcda54b5db991367d46b813c0c7099d1
Elson Otake <elsonotake@yahoo.com.br>
2023-07-04 18:49:42 -0300

    Add before billing check

--- app/controllers/billing_controller.rb
+++ app/controllers/billing_controller.rb
@@ -1,5 +1,9 @@
 class BillingController < ApplicationController
   def show
+    return unless current_user
+    
+    return if current_user.payment_processor.nil?
+
     @portal_session = current_user.payment_processor.billing_portal
   end
 end

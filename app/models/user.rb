ea5613585d7942d0e2ffc744afd4892b2e45dd99
Elson Otake <elsonotake@yahoo.com.br>
2023-07-04 11:17:19 -0300

    Refactor processor definition

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-  include Pay::Billable
+  pay_customer default_payment_processor: :stripe
+  # include Pay::Billable
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,

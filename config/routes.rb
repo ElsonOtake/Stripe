0fc6d8785b0af790fd723adff00460b19abbbbf2
Elson Otake <elsonotake@yahoo.com.br>
2023-07-04 09:48:44 -0300

    Add routes to checkout and billing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :products
-  post "/checkout", to: "checkout#create"
+  # post "/checkout", to: "checkout#create"
+  get "/checkout", to: "checkout#show"
+  get "/billing", to: "billing#show"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

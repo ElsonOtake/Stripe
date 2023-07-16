fabe7f38db3fdd89b860a611a5688743046abfb1
Elson Otake <elsonotake@yahoo.com.br>
2023-07-16 15:55:34 -0300

    Remove unused controller

--- app/controllers/billing_controller.rb
+++
@@ -1,9 +0,0 @@
-class BillingController < ApplicationController
-  def show
-    return unless current_user
-
-    return if current_user.payment_processor.nil?
-
-    @portal_session = current_user.payment_processor.billing_portal
-  end
-end

2ad6d88114001674722db242b24adf1ee458057c
Elson Otake <elsonotake@yahoo.com.br>
2023-07-16 10:51:06 -0300

    Rename js hello controller to stimulus

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,5 +4,5 @@
 
 import { application } from "./application"
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import StimulusController from "./stimulus_controller"
+application.register("stimulus", StimulusController)

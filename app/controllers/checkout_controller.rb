aaa6c2827855e1c71f9d282bd205fdb2f5b60852
Elson Otake <elsonotake@yahoo.com.br>
2023-07-16 18:41:54 -0300

    Update success page style

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,13 +1,12 @@
 class CheckoutController < ApplicationController
   def show
-    # current_user.set_payment_processor :stripe
-    # current_user.payment_processor.customer
     @price = Stripe::Price.retrieve(params[:line_items])
     @product = Stripe::Product.retrieve(@price.product)
     @checkout_session = current_user.payment_processor.checkout(
       mode: params[:payment_mode],
       line_items: params[:line_items],
-      success_url: checkout_success_url
+      success_url: checkout_success_url,
+      cancel_url: checkout_cancel_url,
     )
   end
   
@@ -26,17 +25,15 @@ class CheckoutController < ApplicationController
         quantity: 1,
       }],
       success_url: checkout_success_url,
-      cancel_url: root_url,
+      cancel_url: checkout_cancel_url,
     )
   end
 
   def success
-    # Stripe::Checkout::Session.methods
     @session = Stripe::Checkout::Session.retrieve(params[:session_id])
     @line_items = Stripe::Checkout::Session.list_line_items(params[:session_id])
     payment_intent = Stripe::PaymentIntent.retrieve(@session.payment_intent)
     @charge = Stripe::Charge.retrieve(payment_intent.latest_charge)
-    @payment_method = Stripe::PaymentMethod.retrieve(payment_intent.payment_method)
   end
 
   def cancel

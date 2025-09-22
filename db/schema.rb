beacf5c2322222b81bc23ebbe78025300e2ac081
Elson Otake <elsonotake@yahoo.com.br>
2025-09-22 19:47:05 -0300

    upgrate rails to version 8

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_05_210606) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_22_222623) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "pay_charges", force: :cascade do |t|
     t.bigint "customer_id", null: false
